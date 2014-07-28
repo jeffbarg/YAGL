a4ccc44345d4113d64f0ab51bdcd0b47f2ba8d60
Edgar Aroutiounian <edgar.factorial@gmail.com>
2014-07-28 13:10:53 -0400

    small clean up

--- src/FrontEnd/output_test.ml
+++ src/FrontEnd/output_test.ml
@@ -1,9 +1,11 @@
-
-
 let main () = 
-(* Flexible input, either as piped in or a stdin *)
-  let lexbuf = if Sys.argv > 1 
-	       then Sys.argv.(1)
-	       else Lexing.from_channel stdin in 
-  let output = Parser.prog Scanner.main_entry lexbuf in 
-  print_endline "test"
+  let cin = 
+    if Array.length Sys.argv > 1
+    then open_in Sys.argv.(1)
+    else stdin 
+  in 
+  let lexbuf = Lexing.from_channel cin in  
+  Scanner.main_entry lexbuf
+(* since there are no printfs in the actions, 
+   this won't give anything meaningful right now *)
+let _ = Printexc.print main () 
