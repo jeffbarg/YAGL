874cfdb2767eb86fd82ab9710e682a9f72e8c675
Edgar Aroutiounian <edgar.factorial@gmail.com>
2014-08-15 12:54:11 -0400

    Got runyagl up and running correctly

--- runyagl.sh
+++ runyagl.sh
@@ -4,12 +4,9 @@ make >/dev/null 2>&1
 #NAME=`echo "$1" | cut -d'.' -f1`
 
 filename=$(basename "$1")
-#filename="${filename%.*}"
+filename="${filename%.*}"
 
-g++ -g -v -Wall -std=c++11 "example.cpp" -o $filename -L/cpp/json#>/dev/null 2>&1
-# NOTE, g++ builtin_functions.cpp -I../dist/ -L../dist/
-#chmod a+x $filename
-#./$filename
-
+g++ -v -Wall -std=c++11 "example.cpp" -o $filename -I./cpp >/dev/null 2>&1
+rm -f example.cpp
 #open $filename".svg"
 
